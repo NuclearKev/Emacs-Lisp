c5e3b52d52e554357720f219d3bf47a4dc63c8ba
Kevin Bloom <13bloom.kevin@gmail.com>
2016-01-07 18:10:51 -0500

    Wrote my first decent elisp function

---
+++ elisp-fun.el
@@ -0,0 +1,16 @@
+;; This file is Free Software under the GNU GPLv3.
+
+;; One day this will contain a bunch of fun Elisp functions that might help
+;; some sad sack someday. Enjoy.
+
+(defun replace-a-word (org-word new-word)
+  "Replace a word with a different one!"
+  (interactive "sWord to replace:\nsNew word:") ;\n allows for [RET] between
+  (let ((found-flag nil))
+    (goto-char (point-min))
+    (while (integerp (search-forward org-word nil "done"))
+      (setq found-flag t)		;not very good practice..
+      (replace-match new-word))
+    (if found-flag
+	(message "Replaced!")
+      (message "The word %s wasn't found!" org-word))))
